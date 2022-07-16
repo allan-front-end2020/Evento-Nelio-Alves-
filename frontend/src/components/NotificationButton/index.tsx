40dd31b925d0656a2b52eff80e680c9a5bd7c15c
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-07-16 16:24:36 -0300

    alerta de envio de smms feito

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,22 @@
+import axios from "axios";
+import { toast } from "react-toastify";
 import icon from  "../../assets/img/Vector (1).svg";
+import { BASE_URL } from "../../utils/resquest";
 import "./style.css"
 
+type Prosp ={
+    saleId:number;
+}
+function handleClick(id : number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response =>{
+       toast .info("Sms enviado com sucesso")
+    });
+}
 
-function NotificationButton() {
+function NotificationButton({saleId} : Prosp) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
          <img src={icon} alt="Notificar"/>
         </div>
     )
